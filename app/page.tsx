bd547b7e6c736a701f8ec56077f6c7f178dd8f88
joeulam <joeulam345@gmail.com>
2024-11-02 12:45:51 -0400

    Added First tab

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,27 @@
 'use client'
-import Image from "next/image";
-import { createTheme, MantineProvider } from '@mantine/core';
-import { NavLink } from '@mantine/core';
-import '@mantine/core/styles.css';
+import {MantineProvider } from '@mantine/core';
 import { IconChefHat } from '@tabler/icons-react'
-import { Burger } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
+import { Tabs } from '@mantine/core';
+import '@mantine/core/styles.css';
+
 
 export default function Home() {
-  const [opened, { toggle }] = useDisclosure();
-
   return (
     <MantineProvider defaultColorScheme="dark">
       <div>
         <h1>BU Fit tracker</h1>
-        <Burger opened={opened} onClick={toggle} aria-label="Toggle navigation" />
       </div>
-      <NavLink
-        href="#required-for-focus"
-        label="West Dining hall"
-        leftSection={<IconChefHat size="1rem" stroke={1.5} />}
-      />
+      <Tabs defaultValue="West_Dining_hall">
+        <Tabs.List>
+          <Tabs.Tab value="West_Dining_hall" leftSection={<IconChefHat/>}>
+            West Dining hall
+          </Tabs.Tab>
+        </Tabs.List>
+
+        <Tabs.Panel value="West_Dining_hall">
+          Menu
+        </Tabs.Panel>
+      </Tabs>
     </MantineProvider>
   );
 }
