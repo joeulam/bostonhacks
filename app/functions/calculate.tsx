2c06e35900ef9ee282812c46f4994d46cd55e6f9
diggygeorge <diggyzar@bu.edu>
2024-11-02 23:09:20 -0400

    just trying to get rid of the commit thing

--- app/functions/calculate.tsx
+++ app/functions/calculate.tsx
@@ -24,7 +24,7 @@ interface FoodItem {
 }
 
 // Use require to load the JSON data
-const items = require(`../functions/json/food_items_marci.json`); 
+const items: FoodItem = require(`../functions/json/food_items_marci.json`); 
 
 export function Calculate({ cartItems, removeFromCart }: CalculateProps) {
   const [caloriesMap, setCaloriesMap] = useState<{ [key: string]: number }>({}); // State for calories
@@ -35,7 +35,7 @@ export function Calculate({ cartItems, removeFromCart }: CalculateProps) {
   useEffect(() => {
     const initialCaloriesMap: { [key: string]: number } = {};
     cartItems.forEach(item => {
-      const foodItem = items.find((dataItem: { id: string; }) => dataItem.id.trim() === item.trim());
+      const foodItem = items.find((dataItem: typeof foodItem) => dataItem.id.trim() === item.trim());
       if (foodItem) {
         initialCaloriesMap[item] = foodItem.Calories;
       }
@@ -60,7 +60,7 @@ export function Calculate({ cartItems, removeFromCart }: CalculateProps) {
   }
 
   function handleCardClick(itemName: string) {
-    const item = items.find((dataItem : any) => dataItem.id.trim() === itemName.trim());
+    const item = items.find((dataItem) => dataItem.id.trim() === itemName.trim());
     if (item) {
       setSelectedItem(item); // Set the selected item for modal
       setModalOpened(true); // Open the modal
